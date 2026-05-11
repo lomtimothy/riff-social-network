f924dad6987e877c356d695e923339772ad12274
Alvarado Lomeli Gael Ramses <alvaradogael576@gmail.com>
2026-05-11 13:37:17 -0600

    contactos

--- users/context_processors.py
+++ users/context_processors.py
@@ -6,7 +6,7 @@ def social_data(request):
         solicitudes = FriendRequest.objects.filter(receiver=request.user)
         
         # 2. Obtenemos sugerencias (excluimos a ti mismo, a tus amigos y a los que ya tienen solicitud)
-        amigos = request.user.friends.all()
+        amigos = request.user.friends.all() # ¡Esta es la variable clave!
         enviadas = FriendRequest.objects.filter(sender=request.user).values_list('receiver_id', flat=True)
         recibidas = FriendRequest.objects.filter(receiver=request.user).values_list('sender_id', flat=True)
         
@@ -14,10 +14,13 @@ def social_data(request):
                                   .exclude(id__in=amigos)\
                                   .exclude(id__in=enviadas)\
                                   .exclude(id__in=recibidas)[:5] # Máximo 5 sugerencias
+        
         mensajes_sin_leer = Message.objects.filter(receiver=request.user, is_read=False).count()
+        
         return {
             'solicitudes_pendientes': solicitudes,
             'sugerencias_amistad': sugerencias,
-            'mensajes_sin_leer': mensajes_sin_leer
+            'mensajes_sin_leer': mensajes_sin_leer,
+            'amigos_activos': amigos  # <--- ESTA ES LA SOLUCIÓN MAGICA
         }
     return {}
