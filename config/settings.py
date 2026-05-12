eaf7769276c400a97966ba2a65bc6853f0f672b8
Alvarado Lomeli Gael Ramses <alvaradogael576@gmail.com>
2026-05-12 01:12:38 -0600

    Cambiar envío de correos a la consola para evitar bloqueo de Render

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ LOGOUT_REDIRECT_URL = 'login'
 LOGIN_URL = '/cuentas/login/'
 
 # CONFIGURACIÓN DE CORREO (SMTP GMAIL)
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
